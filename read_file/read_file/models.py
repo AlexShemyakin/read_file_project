f4edd90b5144956ba45a7cfc5b5c95f5989a81e3
Alexander Shemyakin <64311224+AlexShemyakin@users.noreply.github.com>
2024-04-04 09:06:21 +0300

    Update models.py

--- read_file/read_file/models.py
+++ read_file/read_file/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class UploadedFile(models.Model):
-    """Форма загрузки файла."""
+    """Модель загрузочного файла."""
     file = models.FileField(upload_to='uploaded_files/')
     upload_date = models.DateTimeField(auto_now_add=True)
